3d060634d6bf04f913937f8a7d996cd8bd5a8499
neiljo10 <offwork03@gmail.com>
2021-11-05 21:39:07 +0530

    Adding JAVA code to Subtract and updating README.md file

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ MyFirst Git Project. Thanks Ma'am for This Opportunity.
 * Compare the temporary number with reversed number
 * If both numbers are same, print "palindrome number"
 * Else print "not palindrome number"
+
+#Added Subtract Program in JAVA
+It tells the difference when 50 is removed from 100
---
+++ Subtract.java
@@ -0,0 +1,15 @@
+public class Subtract {
+
+  public static void main(String[] args) {
+    
+    System.out.println("We will remove 50 from 100 and tell the difference");
+    int first = 100;
+    int second = 50;
+    
+    System.out.println(first + " " + second);
+
+    // Subtract two numbers
+    int diff = first - second;
+    System.out.println("The difference is: " + diff);
+  }
+}
